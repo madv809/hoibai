f062a4aa7bc34cb94af779f3760da9555941f070
madv809 <62539108+madv809@users.noreply.github.com>
2021-12-09 13:49:34 +0700

    Add files via upload

--- cay_VOI.cpp
+++ cay_VOI.cpp
@@ -15,166 +15,119 @@
 #define pb push_back
 #define ef else if
 using namespace std;
-const int mxn = 2e5 + 5;
-const int mxk = 4e5 + 5;
+const int mxn = 1e2 + 5;
+const int mxk = 2e4 + 155;
 const LL INF = 1e15;
 const int lg = 16;
 const LL base = 311;
 const LL MOD = 1e9 + 7;
-vector<int> a[mxn], spec;
-vector<LL> c[mxn];
-int seg[4*mxn][2], node_dp[mxn], n, m, k, nn;
-pLL min_path[mxn];
-pLL path[25][25], dp[25][1 << 20];
-bool vis[25][1 << 20];
+vector<int> a[mxk], target[mxk], op[mxk];
+LL match[mxn][mxn], win[mxn], max_win[mxn], f[4*mxk], c[4*mxk], global_match;
+int d[mxk], n, m, nn, s, t, st;
 
-void creat_node_seg(int l, int r, int par2, int par3, int pos)
+void de(int u, int v, LL cap)
 {
-    if (l == r)
-    {
-        seg[pos][0] = seg[pos][1] = l;
-        a[l].pb(par2);
-        c[l].pb(0LL);
-        a[par3].pb(l);
-        c[par3].pb(0);
-        return;
-    }
-    seg[pos][0] = ++nn;
-    seg[pos][1] = ++nn;
-    a[seg[pos][0]].pb(par2);
-    c[seg[pos][0]].pb(0LL);
-    a[par3].pb(seg[pos][1]);
-    c[par3].pb(0LL);
+    ++m;
+    a[u].pb(m);
+    target[u].pb(v);
+    op[u].pb(m + 1);
+    c[m] = cap;
 
-    int mid = (l + r)>>1;
-    creat_node_seg(l, mid, seg[pos][0], seg[pos][1], pos << 1);
-    creat_node_seg(mid + 1, r, seg[pos][0], seg[pos][1], pos << 1 | 1);
+    ++m;
+    a[v].pb(m);
+    target[v].pb(u);
+    op[v].pb(m - 1);
+    c[m] = 0;
 }
 
-void creat_edge_type2(int l, int r, int L, int R, int pos, int u, LL w)
+void creat_graph()
 {
-    if (r < L || R < l) return;
-    if (L <= l && r <= R)
-    {
-        a[seg[pos][0]].pb(u);
-        c[seg[pos][0]].pb(w);
-        return;
-    }
+    s = m = 0;
+    nn = n;
 
-    int mid = (l + r)>>1;
-    creat_edge_type2(l, mid, L, R, pos << 1, u, w);
-    creat_edge_type2(mid + 1, r, L, R, pos << 1 | 1, u, w);
+    REP(i, 1, n) REP(j, i + 1, n)
+    {
+        ++nn;
+        de(nn, j, match[i][j]);
+        de(nn, i, match[j][i]);
+
+        ++m;
+        a[s].pb(m);
+        target[s].pb(nn);
+        op[s].pb(-1);
+        c[m] = match[i][j];
+    }
+    t = ++nn;
+
+    st = m + 1;
+    REP(i, 1, n)
+    {
+        ++m;
+        a[i].pb(m);
+        target[i].pb(t);
+        op[i].pb(-1);
+        c[m] = -win[i];
+    }
 }
 
-void creat_edge_type3(int l, int r, int L, int R, int pos, int u, LL w)
+bool bfs()
 {
-    if (r < L || R < l) return;
-    if (L <= l && r <= R)
+    REP(i, 0, nn) d[i] = 0;
+    d[s] = 1;
+    deque<int> Q;
+    Q.push_back(s);
+
+    while(Q.size())
     {
-        a[u].pb(seg[pos][1]);
-        c[u].pb(w);
-        return;
+        int u = Q.front();
+        if (u == t) return 1;
+        Q.pop_front();
+
+        FOR(i, 0, (int)a[u].size())
+        {
+            int x = a[u][i], v = target[u][i];
+            if (f[x] >= c[x] || d[v]) continue;
+            d[v] = d[u] + 1;
+            Q.pb(v);
+        }
     }
 
-    int mid = (l + r)>>1;
-    creat_edge_type3(l, mid, L, R, pos << 1, u, w);
-    creat_edge_type3(mid + 1, r, L, R, pos << 1 | 1, u, w);
-}
-
-bool mini(pLL &a, pLL b)
-{
-    if (a.X > b.X) {a = b; return 1;}
-    ef (a.X == b.X) (a.Y += b.Y)%=MOD;
     return 0;
 }
 
-//type2 là hội tụ
-//type3 là phân tán
-void creat_graph()
+LL dfs(int u, LL max_flow)
 {
-    cin >> n >> m >> k;
-    REP(i, 1, n) node_dp[i] = -1;
-    FOR(i, 0, k)
+    if (u == t) return max_flow;
+    FOR(i, 0, (int)a[u].size())
     {
-        int u; cin >> u;
-        node_dp[u] = i;
-        spec.pb(u);
-    }
-    if (node_dp[1] == -1) node_dp[1] = k;
-
-    int t, u, v, l, r; LL w;
-    nn = n;
-    creat_node_seg(1, n, 0, 0, 1);
-    REP(i, 1, m)
-    {
-        cin >> t;
-        if (t == 1)
+        int x = a[u][i], v = target[u][i];
+        if (f[x] >= c[x] || d[v] != d[u] + 1) continue;
+        LL tt = dfs(v, min(max_flow, c[x] - f[x]));
+        if (tt)
         {
-            cin >> u >> v >> w;
-            a[u].pb(v);
-            c[u].pb(w);
-        }
-        ef (t == 2)
-        {
-            cin >> l >> r >> u >> w;
-            creat_edge_type2(1, n, l, r, 1, u, w);
-        }
-        else
-        {
-            cin >> u >> l >> r >> w;
-            creat_edge_type3(1, n, l, r, 1, u, w);
+            f[x] += tt;
+            if (op[u][i] != -1) f[op[u][i]] -= tt;
+            return tt;
         }
     }
-
-    if (node_dp[1] == k) spec.pb(1);
-    for (int u : spec) for (int v : spec) path[node_dp[u]][node_dp[v]] = {INF, INF};
-    //tính path[u][v]
-    priority_queue<pLi, vector<pLi>, greater<pLi> >heap;
-    for (int tt : spec)
-    {
-        REP(i, 1, nn) min_path[i] = {INF, 0};
-        min_path[tt] = {0, 1};
-        heap.push({0, tt});
-
-        while(heap.size())
-        {
-            w = heap.top().X; u = heap.top().Y;
-            heap.pop();
-            if (w != min_path[u].X) continue;
-            for (int i = 0; i < (int)a[u].size(); ++i)
-            {
-                v = a[u][i]; w = c[u][i];
-                if (mini(min_path[v], {min_path[u].X + w, min_path[u].Y}))
-                    heap.push({min_path[v].X, v});
-            }
-        }
-
-        for (int v : spec) if (v != tt)
-            path[node_dp[tt]][node_dp[v]] = min_path[v];
-        else path[node_dp[tt]][node_dp[v]] = {0, 1};
-    }
+    return 0;
 }
 
-void mini2 (pLL &a, pLL b)
+bool check(int u)
 {
-    if (a.X > b.X) a = b;
-    ef (a.X == b.X) (a.Y += b.Y)%=MOD;
-}
+    REP(i, st, m) c[i] += max_win[u];
+    REP(i, 1, m) f[i] = 0;
 
-//dp bit mask
-pLL solve(int u, int mask)
-{
-    if (vis[u][mask]) return dp[u][mask];
-    vis[u][mask] = 1;
-    pLL &ans = dp[u][mask];
-    ans = {INF, INF};
-
-    FOR(i, 0, k) if ((mask >> i & 1) && path[u][i].X != INF)
+    LL res = 0, x;
+    while(bfs())
     {
-        pLL x = solve(i, mask^(1 << i));
-        mini2(ans, {path[u][i].X + x.X, path[u][i].Y*x.Y%MOD});
+        while(x = dfs(s, INF))
+            res += x;
     }
-    return ans;
+
+    REP(i, st, m) c[i] -= max_win[u];
+    if (res == global_match) return 1;
+    return 0;
 }
 
 int main()
@@ -182,21 +135,44 @@ int main()
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
     ios::sync_with_stdio(false); cin.tie(NULL);
-    
-    // tạo đồ thị
-    creat_graph();
-    
-    // spec là tập các đỉnh cần được phát quà
-    for (int u : spec)
+    int t; cin >> t;
+
+    while(t--)
     {
-        vis[node_dp[u]][0] = 1;
-        dp[node_dp[u]][0] = path[node_dp[u]][node_dp[1]];
+        cin >> n;
+        REP(i, 1, n)
+        {
+            max_win[i] = 0;
+            REP(j, 1, n)
+            {
+                cin >> match[i][j];
+                max_win[i] += match[i][j];
+            }
+        }
+
+        LL mx = global_match = 0;
+        REP(i, 1, n)
+        {
+            cin >> win[i]; mx = max(mx, win[i]);
+            global_match += max_win[i];
+            max_win[i] += win[i];
+        }
+        global_match >>= 1;
+
+        creat_graph();
+        //cout << nn << ' ' << m; return 0;
+
+        vector<int> res;
+        REP(i, 1, n) if (max_win[i] >= mx && check(i))
+            res.pb(i);
+
+        cout << res.size() << ' ';
+        for (int x : res) cout << x << ' '; cout << "\n";
+        REP(i, 0, nn)
+        {
+            a[i].clear();
+            target[i].clear();
+            op[i].clear();
+        }
     }
-    
-    // dp bit mask 
-    int mask = (1 << k) - 1;
-    if (node_dp[1] != k && node_dp[1] != -1) mask^=(1 << node_dp[1]);
-    pLL x = solve(node_dp[1], mask);
-    if (x.X == INF) cout << "-1";
-    else cout << x.X << ' ' << x.Y;
 }
