85a0aa3bca93609f86b086b738f6fbcff7b2bf2e
madv809 <62539108+madv809@users.noreply.github.com>
2022-02-03 00:01:33 +0700

    Update cay_VOI.cpp

--- cay_VOI.cpp
+++ cay_VOI.cpp
@@ -1,8 +1,10 @@
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include<bits/stdc++.h>
 #define LL long long
 #define RED(i, a, b) for (int i = (a); i >= (b); --i)
 #define REP(i, a, b) for(int i = (a); i <= (b); ++i)
-#define FOR(i, a, b) for(int i = (a); i <= (b - 1); ++i)
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
 #define pii pair<int, int>
 #define pLL pair<LL, LL>
 #define piL pair<int, LL>
@@ -14,171 +16,172 @@
 
 #define pb push_back
 #define ef else if
+//#define int LL
 using namespace std;
-const int mxn = 2e5 + 5;
-const int mxk = 1e7;
-const int S = 200;
-const long double eps = 1e-10;
-const int INF = 1e9;
-const int lg = 16;
+const int N = 1e2 + 51;
+const int K = 1e2 + 5;
+const int mxk = 5e5 + 5;
+const LL INF = 1e18;
+const int lg = 40;
 const LL base = 311;
-const LL MOD = 1e9 + 7;
-vector<int> a[mxn], sta;
-vector<pii> bridge;
-int par[lg + 1][mxn], st[mxn], ed[mxn], bit[mxn], num[mxn], low[mxn], d[mxn], comp[mxn], ti, t, n, m;
-
-void up(int x, int val)
-{
-    while(x <= n)
-    {
-        bit[x] += val;
-        x += x&-x;
-    }
-}
-
-void update(int l, int r, int val)
-{
-    up(l, val);
-    up(r + 1, -val);
-}
-
-int get(int x)
-{
-    int res = 0;
-
-    while(x > 0)
-    {
-        res += bit[x];
-        x -= x&-x;
-    }
-    return res;
-}
-
-void dfs_make_comp(int u, int p)
-{
-    num[u] = low[u] = ++ti;
-    for (int v : a[u]) if (v != p)
-    {
-        if (num[v]) low[u] = min(low[u], num[v]);
-        else
-        {
-            dfs_make_comp(v, u);
-            low[u] = min(low[u], low[v]);
-        }
-    }
-    sta.pb(u);
-
-    if (num[u] == low[u])
-    {
-        ++n;
-        bridge.pb({p, u});
-
-        while(sta.size())
-        {
-            comp[sta.back()] = n;
-            sta.pop_back();
-        }
-    }
-}
-
-void dfs(int u, int p)
-{
-    st[u] = ++t;
-    for (int v : a[u]) if (v != p)
-    {
-        par[0][v] = u;
-        d[v] = d[u] + 1;
-        dfs(v, u);
-    }
-
-    ed[u] = t;
-}
+const LL inv_base = 787781356;
+//const LL MOD = 1e3;
+const int MAX = 1e6;
+vector<int> tt[N];
+int m, n, MOD, S;
+int dp[N][N][N][41], nxt[41][N], child[41][N], f[41], par[41], node, end_state;
+bool ok[41];
+string grid[N], xau[41], xuoi, nguoc;
 
 void calc_something()
 {
-    cin >> n >> m;
-    int u, v;
+    cin >> m >> n >> MOD >> xuoi;
     REP(i, 1, m)
     {
-        cin >> u >> v;
-        a[u].pb(v); a[v].pb(u);
+        cin >> grid[i];
+        grid[i] = '#' + grid[i];
     }
+    S = xuoi.size();
+    nguoc = xuoi;
 
-    n = 0;
-    dfs_make_comp(1, 0);
-    REP(u, 1, n) a[u].clear();
-    FOR(i, 0, (int)bridge.size())
+    reverse(nguoc.begin(), nguoc.end());
+    xuoi = '#' + xuoi;
+    nguoc = '#' + nguoc;
+    xau[0] = '#';
+
+    REP(i, 1, S)
     {
-        u = comp[bridge[i].X]; v = comp[bridge[i].Y];
-        if (u == 0) continue;
-        a[u].pb(v); a[v].pb(u);
+        par[i] = i - 1;
+        child[i - 1][xuoi[i]] = i;
+        xau[i] = xau[i - 1] + xuoi[i];
+        tt[i].pb(i);
     }
 
-    dfs(1, 0);
-    REP(i, 1, lg) REP(u, 1, n)
-        par[i][u] = par[i - 1][par[i - 1][u]];
-    st[0] = -INF; ed[0] = INF;
-
-    REP(u, 1, n) update(st[u], st[u], d[u]);
-}
-
-bool is_par(int u, int v)
-{
-    return (st[u] <= st[v] && ed[u] >= ed[v]);
-}
-
-int lca(int u, int v)
-{
-    if (is_par(u, v)) return u;
-    if (is_par(v, u)) return v;
-
-    RED(i, lg, 0) if (!is_par(par[i][u], v))
-        u = par[i][u];
-
-    return par[0][u];
-}
-
-int find_next(int u)
-{
-    int x = get(st[u]);
-    RED(i, lg, 0) if (get(st[par[i][u]]) == x)
-        u = par[i][u];
-
-    if (u == 1) return 0;
-    return u;
-}
-
-void query(int u, int x)
-{
-    while(1)
+    node = S;
+    REP(i, 1, S) if (xuoi[i] != nguoc[i])
     {
-        u = find_next(u);
-        if (is_par(u, x)) return;
-        update(st[u], ed[u], -1);
+        ++node;
+        par[node] = i - 1;
+        child[i - 1][nguoc[i]] = node;
+        xau[node] = xau[i - 1] + nguoc[i];
+        tt[i].pb(node);
+
+        REP(t, i + 1, S)
+        {
+            ++node;
+            par[node] = node - 1;
+            child[node - 1][nguoc[t]] = node;
+            xau[node] = xau[node - 1] + nguoc[t];
+            tt[t].pb(node);
+        }
+        break;
     }
+
+    REP(i, 1, S) for (int x : tt[i])
+    {
+        if (par[x] == 0) continue;
+        int t = f[par[x]];
+        while(t > 0 && child[t][xau[x][i]] == 0)
+            t = f[t];
+        f[x] = child[t][xau[x][i]];
+    }
+
+    REP(i, 0, node) for (char ch = 'a'; ch <= 'z'; ++ch)
+    {
+        int t = i;
+        while(t > 0 && child[t][ch] == 0)
+            t = f[t];
+        nxt[i][ch] = child[t][ch];
+    }
+    int ed1 = 0, ed2 = 0;
+    ed1 = S;
+    ed2 = node;
+
+    for (char ch = 'a'; ch <= 'z'; ++ch)
+    {
+        nxt[ed1][ch] = ed1;
+        nxt[ed2][ch] = ed2;
+    }
+
+    REP(t, 1, node) for(int i : tt[t])
+    {
+        ok[i] = ok[f[i]];
+        if (ok[i]) continue;
+        string t = xau[i];
+        reverse(xau[i].begin(), xau[i].end());
+        t += xau[i];
+        while(t.size() > xuoi.size())
+            t.erase((t.size()) - 1, 1);
+
+        if (t == xuoi || t == nguoc)
+            ok[i] = 1;
+    }
+    ok[ed1] = ok[ed2] = 1;
+
+    end_state = (m + n)/2;
 }
 
-int main()
+bool valid(int x, int y, int u, int v)
 {
+    if (x > u || y > v) return 0;
+    if (x < 1 || x > m || y < 1 || y > n) return 0;
+    if (u < 1 || u > m || v < 1 || v > n) return 0;
+    if ((grid[x][y] != grid[u][v]) || (grid[x][y] == '#')) return 0;
+    return 1;
+}
+
+// dp[len][trai][phai][f]
+int solve(int len, int x, int y, int u, int v, int match)
+{
+    if (len == end_state)
+        return ok[match];
+    int &ans = dp[len][x][u][match];
+    if (ans != -1) return ans;
+    ans = 0;
+
+    if (valid(x, y + 1, u, v - 1))
+        ans += solve(len + 1, x, y + 1, u, v - 1, nxt[match][grid[x][y + 1]]);
+
+    if (valid(x, y + 1, u - 1, v))
+        (ans += solve(len + 1, x, y + 1, u - 1, v, nxt[match][grid[x][y + 1]]))%=MOD;
+
+    if (valid(x + 1, y, u, v - 1))
+        (ans += solve(len + 1, x + 1, y, u, v - 1, nxt[match][grid[x + 1][y]]))%=MOD;
+
+    if (valid(x + 1, y, u - 1, v))
+        (ans += solve(len + 1, x + 1, y, u - 1, v, nxt[match][grid[x + 1][y]]))%=MOD;
+
+    return ans;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
-    ios::sync_with_stdio(false); cin.tie(NULL);
     calc_something();
-
-    int u, v, t, x;
-    while(cin >> t)
+    if (grid[1][1] != grid[m][n])
+        cout << "0";
+    else
     {
-        if (t == 0) break;
-        cin >> u >> v;
-        u = comp[u]; v = comp[v];
-        x = lca(u, v);
-        if (t == 2)
+        memset(dp, -1, sizeof(dp));
+        int t = 0;
+        if (xuoi[1] == nguoc[1])
         {
-            cout << (get(st[u]) + get(st[v]) - 2*get(st[x])) << "\n";
-            continue;
+            if (grid[1][1] == xuoi[1])
+                t = 1;
+            else
+                t = 0;
         }
-
-        query(u, x);
-        query(v, x);
+        else
+        {
+            if (grid[1][1] == xuoi[1])
+                t = 1;
+            ef (grid[1][1] == nguoc[1])
+                t = S + 1;
+            else
+                t = 0;
+        }
+        cout << solve(1, 1, 1, m, n, t);
     }
 }
